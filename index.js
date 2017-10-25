4e005cee1fa02f6cc7740570af912520047e47d2
Tashan O'Neal <tdoneal91@gmail.com>
2017-10-25 07:40:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 const app = "I don't do much."
 var Arrays = ["Milo", "Otis", "Garfield"]
+
+function kittens() {
+  var kittens = ["Milo", "Otis", "Garfield"]
+  return kittens
+}
+function destructivelyAppendKitten(name) {
+  kittens.push("Ralph")
+  return kittens
+}
